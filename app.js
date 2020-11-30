d43229221a23449c4d366a9333c84e455819f6bd
MelihErpek <61746050+MelihErpek@users.noreply.github.com>
2020-11-30 20:59:37 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+const bodyParser = require("body-parser")
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const bodyParser2 = require("body-parser")
+const Article = require("./models/Article")
+app.use(bodyParser2.json())
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://melihnode:meliherpek1@cluster0.g1oel.mongodb.net/<dbname>?retryWrites=true&w=majority";
+let link;
+
+
+mongoose.connect("mongodb+srv://melihnode:meliherpek1@cluster0.g1oel.mongodb.net/urlShorter?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true},
+(err) =>{ if(err) { throw err} console.log("mongoose ile bağlantı kuruldu")}
+   
+);
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+'/views/index.html')
+})
+app.get("/:sL",(req,res)=>{
+    const sL = req.params.sL;
+    
+    MongoClient.connect(uri,{useNewUrlParser: true,useUnifiedTopology:true},function(err,db){
+        if(err) throw err;
+        var dbo = db.db("urlShorter");
+        dbo.collection("examples").findOne({  shortLink : sL },(err,result)=>{
+            if(err) throw err;
+            res.redirect('https://'+result.mainLink)
+            })
+        
+    })
+    
+})
+
+app.post('/',(req,res)=>{
+    let oL = req.body.mainLink;
+    let sL = oL[oL.length-3]+Date.now().toString().substring(9,11)+oL[oL.length-5];
+    var url = new Article({
+        mainLink : oL,
+        shortLink : sL  
+    })
+    url.save()
+    res.json(sL)
+})
+
+
+app.listen(3000)
+
